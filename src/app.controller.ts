685f98c48e5c0a6f856b78ed9644890c11b886cb
Petrik Diak <diak@petrik.hu>
2023-03-29 13:39:12 +0200

    commit

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,12 @@
 import { Body, Controller, Get, Post, Render } from '@nestjs/common';
 import { DataSource } from 'typeorm';
+import { Account } from './account.entity';
 import { AppService } from './app.service';
+import { Owner } from './owner.entity';
+import accountDto from './account.dto';
+import ownerDto from './owner.dto';
+import { Delete } from '@nestjs/common';
+import { Param } from '@nestjs/common';
 
 @Controller()
 export class AppController {
@@ -13,5 +19,41 @@ export class AppController {
   @Render('index')
   index() {
     return { message: 'Welcome to the homepage' };
+  }
+
+  @Get('/account')
+  async listAccount() {
+    const repo = this.dataSource.getRepository(Account);
+    return await repo.find();
+  }
+
+  @Get('/owner')
+  async listOwner() {
+    const repo = this.dataSource.getRepository(Owner);
+    return await repo.find();
+  }
+
+  @Post('/account')
+  async postAccount(@Body() account: accountDto) {
+    const Repo = this.dataSource.getRepository(Account);
+    await Repo.save(account);
+  }
+
+  @Post('/owner')
+  async postOwner(@Body() owner: ownerDto) {
+    const Repo = this.dataSource.getRepository(Owner);
+    await Repo.save(owner);
+  }
+
+  @Delete('/account/:id')
+  async deleteAccount(@Param('id') id: number) {
+    const Repo = this.dataSource.getRepository(Account);
+    await Repo.delete(id);
+  }
+
+  @Delete('/owner/:id')
+  async deleteOwner(@Param('id') id: number) {
+    const Repo = this.dataSource.getRepository(Owner);
+    await Repo.delete(id);
   }
 }
